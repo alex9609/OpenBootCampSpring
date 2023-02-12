fa080de4deccd4f393be1d9d08e6721370af80b5
Alex <alrodriguez41@poligran.edu.co>
2023-02-11 22:27:49 -0500

    Crear y GetById creados

--- ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/controller/BookController.java
+++ ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/controller/BookController.java
@@ -2,10 +2,11 @@ package com.example.obrestdatajpa.controller;
 
 import com.example.obrestdatajpa.entities.Book;
 import com.example.obrestdatajpa.repository.BookRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -25,10 +26,30 @@ public class BookController {
     }
 
     //Buscar un solo libro en base a su id
-    public Book findOne(Long id){
-        return bookRepository.findById(id).get();
+
+    @GetMapping("api/book/{id}")
+    public ResponseEntity<Book> findOne(@PathVariable  Long id){
+        Optional<Book> bookOpt = bookRepository.findById(id);
+
+
+        return  bookOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+        //return bookOpt.isPresent() ? ResponseEntity.ok(bookOpt.get()) : ResponseEntity.notFound().build();
+
+        //return bookOpt.orElse(null);
     }
     //Crear un nuevo libro
+    @PostMapping("api/books")
+    public ResponseEntity<String> create(@RequestBody Book book){
+        String message = "";
+        try {
+            bookRepository.save(book);
+            message = "Created";
+        }catch (Exception e){
+            message = "A problem has ocurred";
+        }
+        return ResponseEntity.ok(message);
+    }
 
     //Actualizar un libro existente en base de datos
 
